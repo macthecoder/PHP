79828c634044aad9b823890a4108038e83a1a80c
Maksud R Mamun <maksudrmamun@gmail.com>
2017-05-16 16:39:48 +0600

    conact_form_7_shortcode.php
    This file is about how to design your own contact form 7.

---
+++ conact_form_7_shortcode.php
@@ -0,0 +1,15 @@
+//Creating Shortcode For Contact form Seven 
+/* Contact form seven text field, 
+[text* name_of_the_field class:first_custom_class class:second_custom_class class:third_custom_class placeholder "Placeholder Name"]
+NOTE: You must follow this sequence.Oherwise you will face error which leads no output.
+[email* name_of_the_field class:first_custom_class class:second_custom_class class:third_custom_class placeholder "Placeholder Name"]
+NOTE: You must follow this sequence.Oherwise you will face error which leads no output.
+[submit class:first_custom_class class:second_custom_class class:third_custom_class "Button Label"]
+NOTE: You must follow this sequence.Oherwise you will face error which leads no output.
+*/
+/*
+<div class="features-content">
+  <div class="request-form">[text* u_name class:form-control placeholder "Name"][email* u_email class:form-control placeholder "Email"][submit class:btn class:thm-btn "Request Demo"]
+  </div>
+</div>
+*/
