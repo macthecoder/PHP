f380232eb78d8eb3179cb70440413d51be33034b
Maksud R Mamun <maksudrmamun@gmail.com>
2017-06-12 16:58:59 +0600

    Create vc_mailchimp_wp_form_integration_inova.php

--- vc_mailchimp_wp_form_integration_inova.php
+++ vc_mailchimp_wp_form_integration_inova.php
@@ -27,7 +27,7 @@ Saas Hero Mailchimp Subscriber form
 
 <?php
 /*
-App Subscriber Form
+(Single Individual) App Landing Pages Subscriber Form
 */
 ?>
 <div class="input-group  subcribes">
@@ -35,5 +35,43 @@ App Subscriber Form
     <span class="input-group-btn">
         <button class="btn btn-default btn-submit" type="submit">subscribe</button>
     </span>
+</div>
+
+<?php
+/*
+(Single Individual) SEO Pages Subscriber Form
+*/
+?>
+<div class="input-group  subcribes-two">
+  <input type="email" name="EMAIL" class="form-control memail" placeholder="Enter your email">
+  <span class="input-group-btn">
+    <button class="btn btn-default btn-submit" type="submit">subscribe</button>
+  </span>
+</div>
+
+<?php
+/*
+(To Use Single Individually) Startup Slider Page Subscriber Form
+*/
+?>
+<div class="slider-text st-slider-text">
+  <div class="subcribe">
+    <input type="email" name="EMAIL" id="reg_email" class="form-control memail" placeholder="Enter your email...">
+    <label>
+      <button class="btn sub_btn sub_btn-two" type="submit">subscribe</button>
+    </label>
+  </div>
+</div>
+
+<?php
+/*
+(To Use Single Individually) Saas Page Subscriber Form
+*/
+?>
+<div class="input-group subcribes-saas">
+  <input type="email" name="email" class="form-control memail" placeholder="Enter your email">
+  <span class="input-group-btn">
+    <button class="btn btn-default btn-submit" type="submit">subscribe</button>
+  </span>
 </div>
 
