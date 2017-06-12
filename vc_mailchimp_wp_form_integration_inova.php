1f23264eebc9897e14468f9a424fa9f571e3e638
Maksud R Mamun <maksudrmamun@gmail.com>
2017-06-12 16:01:02 +0600

    vc_mailchimp_wp_form_integration_inova.php

---
+++ vc_mailchimp_wp_form_integration_inova.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Startup Slider Hero Mailchimp Subscriber form
+*/
+?>
+<div class="mailchimp">
+	<div class="subcribe">
+      <input type="email" name="EMAIL" id="reg_email" class="form-control memail" placeholder="Enter your email...">
+      <label>
+        <button class="btn sub_btn sub_btn-two" type="submit">subscribe</button>
+      </label>
+  	</div>
+</div>
+
+
+<?php
+/*
+Saas Hero Mailchimp Subscriber form
+*/
+?>
+<div class="input-group">
+  <input type="email" name="EMAIL" class="form-control memail" placeholder="Enter your email">
+  <span class="input-group-btn">
+    <button class="btn btn-default btn-submit" type="submit">subscribe</button>
+  </span>
+</div>
+
+<?php
+/*
+App Subscriber Form
+*/
+?>
+<div class="input-group  subcribes">
+    <input type="email" name="EMAIL" class="form-control memail" placeholder="Enter your email">
+    <span class="input-group-btn">
+        <button class="btn btn-default btn-submit" type="submit">subscribe</button>
+    </span>
+</div>
+
